ff55937a148605685e499c777d6a13a228257f95
pjdufour-dds <patrick.dufour@dds.mil>
2020-11-25 07:22:42 -0500

    initial comit

---
+++ README.md
@@ -0,0 +1,58 @@
+## Usage
+
+Creates a KMS Key for use with AWS Kinesis.
+
+```hcl
+module "kinesis_kms_key" {
+  source = "dod-iac/kinesis-kms-key/aws"
+
+  name = format("alias/app-%s-kinesis-%s", var.application, var.environment)
+  description = format("A KMS key used to encrypt Kinesis stream records at rest for %s:%s.", var.application, var.environment)
+  tags = {
+    Application = var.application
+    Environment = var.environment
+    Automation  = "Terraform"
+  }
+}
+```
+
+## Terraform Version
+
+Terraform 0.13. Pin module version to ~> 1.0.0 . Submit pull-requests to master branch.
+
+Terraform 0.11 and 0.12 are not supported.
+
+## License
+
+This project constitutes a work of the United States Government and is not subject to domestic copyright protection under 17 USC § 105.  However, because the project utilizes code licensed from contributors and other third parties, it therefore is licensed under the MIT License.  See LICENSE file for more information.
+
+## Requirements
+
+| Name | Version |
+|------|---------|
+| terraform | >= 0.12 |
+| aws | >= 2.55.0 |
+
+## Providers
+
+| Name | Version |
+|------|---------|
+| aws | >= 2.55.0 |
+
+## Inputs
+
+| Name | Description | Type | Default | Required |
+|------|-------------|------|---------|:--------:|
+| description | n/a | `string` | `"A KMS key used to encrypt Kinesis stream records at-rest."` | no |
+| key\_deletion\_window\_in\_days | Duration in days after which the key is deleted after destruction of the resource, must be between 7 and 30 days. | `string` | `30` | no |
+| name | The display name of the alias. The name must start with the word "alias" followed by a forward slash (alias/). | `string` | n/a | yes |
+| tags | Tags applied to the KMS key. | `map(string)` | `{}` | no |
+
+## Outputs
+
+| Name | Description |
+|------|-------------|
+| aws\_kms\_alias\_arn | The Amazon Resource Name (ARN) of the key alias. |
+| aws\_kms\_alias\_name | The display name of the alias. |
+| aws\_kms\_key\_arn | The Amazon Resource Name (ARN) of the key. |
+
---
+++ main.tf
@@ -0,0 +1,98 @@
+/**
+ * ## Usage
+ *
+ * Creates a KMS Key for use with AWS Kinesis.
+ *
+ * ```hcl
+ * module "kinesis_kms_key" {
+ *   source = "dod-iac/kinesis-kms-key/aws"
+ *
+ *   name = format("alias/app-%s-kinesis-%s", var.application, var.environment)
+ *   description = format("A KMS key used to encrypt Kinesis stream records at rest for %s:%s.", var.application, var.environment)
+ *   tags = {
+ *     Application = var.application
+ *     Environment = var.environment
+ *     Automation  = "Terraform"
+ *   }
+ * }
+ * ```
+ *
+ * ## Terraform Version
+ *
+ * Terraform 0.13. Pin module version to ~> 1.0.0 . Submit pull-requests to master branch.
+ *
+ * Terraform 0.11 and 0.12 are not supported.
+ *
+ * ## License
+ *
+ * This project constitutes a work of the United States Government and is not subject to domestic copyright protection under 17 USC § 105.  However, because the project utilizes code licensed from contributors and other third parties, it therefore is licensed under the MIT License.  See LICENSE file for more information.
+ */
+
+data "aws_caller_identity" "current" {}
+
+data "aws_region" "current" {}
+
+data "aws_partition" "current" {}
+
+data "aws_iam_policy_document" "kinesis" {
+  policy_id = "key-policy-kinesis"
+  statement {
+    sid = "Enable IAM User Permissions"
+    actions = [
+      "kms:*",
+    ]
+    effect = "Allow"
+    principals {
+      type = "AWS"
+      identifiers = [
+        format(
+          "arn:%s:iam::%s:root",
+          data.aws_partition.current.partition,
+          data.aws_caller_identity.current.account_id
+        )
+      ]
+    }
+    resources = ["*"]
+  }
+  statement {
+    sid = "Allow Kinesis stream consumers and producers"
+    actions = [
+      "kms:Encrypt",
+      "kms:Decrypt",
+      "kms:ReEncrypt*",
+      "kms:GenerateDataKey*",
+      "kms:DescribeKey"
+    ]
+    effect = "Allow"
+    principals {
+      type = "AWS"
+      identifiers = [
+        "*"
+      ]
+    }
+    resources = ["*"]
+    condition {
+      test     = "StringEquals"
+      variable = "kms:CallerAccount"
+      values   = [data.aws_caller_identity.current.account_id]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "kms:ViaService"
+      values   = ["kinesis.us-west-2.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_kms_key" "kinesis" {
+  description             = var.description
+  deletion_window_in_days = var.key_deletion_window_in_days
+  enable_key_rotation     = "true"
+  policy                  = data.aws_iam_policy_document.kinesis.json
+  tags                    = var.tags
+}
+
+resource "aws_kms_alias" "kinesis" {
+  name          = var.name
+  target_key_id = aws_kms_key.kinesis.key_id
+}
